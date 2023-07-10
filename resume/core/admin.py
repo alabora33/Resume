98dd7c5fbdf38267c58139caeddfcd6fefcfc5c4
alabora33 <aliboravarinli@gmail.com>
2023-07-10 14:07:05 +0300

    refactor: application of general setting

--- resume/core/admin.py
+++ resume/core/admin.py
@@ -3,7 +3,7 @@ from core.models import *
 # Register your models here.
 
 @admin.register(GeneralSetting)
-class GeneralSettingsAdmin(admin.ModelAdmin):
+class GeneralSettingAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'description', 'parameter', 'updated_date', 'created_date']
     search_fields = ['name', 'description', 'parameter']
     list_editable = ['description', 'parameter']
