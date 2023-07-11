b85c1b86a48df13d92d4a9b8b4b5ca2adfaa5ba7
alabora33 <aliboravarinli@gmail.com>
2023-07-11 13:58:15 +0300

    feat: create skill model

--- resume/core/models.py
+++ resume/core/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -80,8 +81,6 @@ class ImageSetting(AbstractModel):
         blank=True,
         upload_to='images/',
     )
-
-
     def __str__(self):
         return f'Image Setting: {self.name}'
 
@@ -89,3 +88,21 @@ class ImageSetting(AbstractModel):
         verbose_name = 'Image Setting'
         verbose_name_plural = 'Image Settings'
         ordering = ('name',)
+
+class Skill(AbstractModel):
+    order = models.IntegerField(
+        default=0,
+        verbose_name='Order'
+    )
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Name',
+        help_text='This is variable of the setting'
+    )
+    percentage = models.IntegerField(
+        default=50,
+        verbose_name='Percentage',
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
